3fa83d70007774d9b1c7efa4f79a0a7a6b10ef60
Rhys <rhys.jones@yahoo.co.nz>
2020-06-11 21:38:31 +1200

    Tweaks to the edit form to clear on successful save

--- src/FlightLog/FlightLog/src/app/flight-editor/flight-editor.component.ts
+++ src/FlightLog/FlightLog/src/app/flight-editor/flight-editor.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormBuilder, Form } from '@angular/forms';
-import { ModelService } from '../model.service'; 
+import { ModelService } from '../model.service';
 import { IModel } from '../interfaces/imodel';
 import { FlightService } from '../flight.service';
 import { IFlight } from '../interfaces/iflight';
@@ -14,7 +14,7 @@ import { Flight } from '../models/flight';
 })
 export class FlightEditorComponent implements OnInit {
 
-  constructor(private formBuilder: FormBuilder, private modelService: ModelService, private flightService: FlightService) { 
+  constructor(private formBuilder: FormBuilder, private modelService: ModelService, private flightService: FlightService) {
     // Get the list of models
     modelService.getModels().subscribe((data: IModel[]) => {
       this.models = data;
@@ -24,31 +24,39 @@ export class FlightEditorComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  
+
   // TODO set up a class that implements flight (type=model) 
   flight: IFlight = new Flight();
   savedFlight: IFlight;
+  message: string;
 
   flightForm = this.formBuilder.group({
     date: [new Date()],
-    flightMinutes: [''], 
-    model: [''],
+    flightMinutes: [''],
+    modelId: [''],
     details: ['']
   });
 
   onSubmit() {
     console.warn(this.flightForm.value)
-    
+
     this.flight.Date = this.flightForm.value.date;
-    this.flight.ModelId = this.flightForm.value.model;
+    this.flight.ModelId = this.flightForm.value.modelId;
     this.flight.FlightMinutes = this.flightForm.value.flightMinutes;
     this.flight.Details = this.flightForm.value.details;
     this.flight.FieldId = 1;  // TODO look it up
 
-    this.flightService.addFlight(this.flight).subscribe((data: IFlight) => { 
-      this.savedFlight = data; 
+    this.flightService.addFlight(this.flight).subscribe((data: IFlight) => {
+      this.savedFlight = data;
       console.log(this.savedFlight);
+      this.message = `Saved flight with Id ${this.savedFlight.Id}.`;
+      this.clearForm();
     });
+  }
+
+  clearForm() {
+    this.flightForm.reset();
+    this.flightForm.patchValue({ date: new Date() });
   }
 
   models: IModel[];
