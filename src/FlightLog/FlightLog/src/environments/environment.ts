17b52cf87b524185a818ed4641fe0c5ccf0d30d5
Rhys <rhys.jones@yahoo.co.nz>
2021-10-03 15:13:28 +1300

    Update prod config for Auth0

--- src/FlightLog/FlightLog/src/environments/environment.ts
+++ src/FlightLog/FlightLog/src/environments/environment.ts
@@ -8,7 +8,11 @@ export const environment = {
   identityServerAuthority: "https://localhost:5001/",
   identityServerRedirectUri: "http://localhost:4200/auth-callback", 
   identityServerPostLogoutRedirectUri: "http://localhost:4200/",
-  silentRedirectUri: "http://localhost:4200/silent-refresh.html"
+  silentRedirectUri: "http://localhost:4200/silent-refresh.html",
+  auth0Domain: "dev-flightlog.au.auth0.com", 
+  auth0ClientId: "K4yvPKBlTCzvhmVSsjPiZCEIGBGbDr8n", 
+  auth0RedirectUri: window.location.origin, 
+  auth0Scope: 'openid profile email'
 };
 
 /*
